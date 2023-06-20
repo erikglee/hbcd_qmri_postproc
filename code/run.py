2b22fd06649056b8d8f0cc92627d0c2b9bd8a097
erikglee <erik.lee.mn@gmail.com>
2023-06-20 12:16:50 -0500

    [dev]

--- code/run.py
+++ code/run.py
@@ -11,8 +11,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("bids_dir", help="The path to the BIDS directory for your study (this is the same for all subjects)", type=str)
 parser.add_argument("output_dir", help="The path to the folder where outputs will be stored (this is the same for all subjects)", type=str)
 parser.add_argument("analysis_level", help="Should always be participant", type=str)
-parser.add_argument("symri_deriv_dir", "The path to the folder where the SyMRI Relaxometry Maps are stored (this is the same for all subjects)", type=str)
-parser.add_argument("bibsnet_deriv_dir", "The path to the folder where the BIBSNET/CABINET segmentations are stored (this is the same for all subjects)", type=str)
+parser.add_argument("symri_deriv_dir", help="The path to the folder where the SyMRI Relaxometry Maps are stored (this is the same for all subjects)", type=str)
+parser.add_argument("bibsnet_deriv_dir", help="The path to the folder where the BIBSNET/CABINET segmentations are stored (this is the same for all subjects)", type=str)
 
 
 parser.add_argument('--participant_label', '--participant-label', help="The name/label of the subject to be processed (i.e. sub-01 or 01)", type=str)
