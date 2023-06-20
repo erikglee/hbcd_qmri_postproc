052ea6e6f42d0764332c4ac7531b6336ea0e4eab
erikglee <erik.lee.mn@gmail.com>
2023-06-20 11:30:16 -0500

    [dev]

--- code/qmri_postproc.py
+++ code/qmri_postproc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 import os, glob
 import nibabel
 import matplotlib.pyplot as plt
--- code/run.py
+++ code/run.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 import numpy as np
 import matplotlib.pyplot as plt
 import qmri_postproc
