b71bd285329f77a879b29b599e7adf260ec71593
Erik Lee <erik.lee.mn@gmail.com>
2024-01-25 10:06:41 -0600

    [dev]

--- code/run.py
+++ code/run.py
@@ -18,6 +18,7 @@ parser.add_argument("bibsnet_deriv_dir", help="The path to the folder where the 
 parser.add_argument('--participant_label', '--participant-label', help="The name/label of the subject to be processed (i.e. sub-01 or 01)", type=str)
 parser.add_argument('--session_id', '--session-id', help="OPTIONAL: the name of a specific session to be processed (i.e. ses-01)", type=str)
 parser.add_argument('--overwrite_existing', help='OPTIONAL: if flag is activated, the tool will delete the session folder where outputs are to be stored before processing if said folder already exists.', action='store_true')
+parser.add_argument('--skip_existing', help='OPTIONAL: if flag is activated, the tool will skip processing for a session if the session folder where outputs are to be stored already exists.', action='store_true')
 parser.add_argument('--region_groupings_json', nargs='+', help='OPTIONAL: the path to a json file containing region groupings for which to calculate statistics. Multiple files can be provided, resulting in multiple output csv files.', type=str)
 args = parser.parse_args()
 
@@ -87,7 +88,10 @@ for temp_participant in participants:
     for temp_session in sessions:
 
         session_path = os.path.join(output_dir, temp_participant, temp_session)
-        if os.path.exists(session_path) and args.overwrite_existing:
+        if os.path.exists(session_path) and args.skip_existing:
+            print('Session folder already exists at the following path. Skipping: ' + session_path)
+            continue
+        elif os.path.exists(session_path) and args.overwrite_existing:
             shutil.rmtree(session_path)
             print('Removing existing session folder at: ' + session_path)
         elif os.path.exists(session_path):
