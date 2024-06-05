be1091294b806e97b48b3ca044033341f90bbc91
erik lee <erik.lee.mn@gmail.com>
2024-06-05 14:20:21 -0500

    [dev]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,24 +9,26 @@
 project = 'HBCD_SYMRI_POSTPROC'
 copyright = '2024, HBCD'
 author = 'HBCD'
-release = '1'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme', 'sphinx-argparse']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx-argparse',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+# If your main document is not index.rst, uncomment and specify it
+# master_doc = 'index'
