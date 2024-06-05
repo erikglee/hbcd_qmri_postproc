84bedb1df0a5ed18c032bfefeff258d7187e0da7
erik lee <erik.lee.mn@gmail.com>
2024-06-05 11:24:17 -0500

    [dev]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,3 +26,7 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'sphinx_rtd_theme'
