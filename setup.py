8f6790c3c65d354bd204398131ebd9978f9054e1
erik lee <erik.lee.mn@gmail.com>
2024-06-05 11:59:24 -0500

    [dev]

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'sphinx',
+        'sphinx_rtd_theme',
+    ],
+)
